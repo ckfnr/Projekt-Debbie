630d21bb8e7d944988d56ef9938fb54c07634333
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2024-12-28 15:58:21 +0100

    Add files via upload

--- Software/test_gyroscope_01.py
+++ Software/test_gyroscope_01.py
@@ -5,5 +5,12 @@ sensor = mpu6050(0x68)
 accel_data: dict[str, float] = sensor.get_accel_data()
 gyro_data: dict[str, float] = sensor.get_gyro_data()
 
-print(f"{"  ".join(key + ": " + str(value) for key, value in accel_data.items())}")
-print(f"{"  ".join(key + ": " + str(value) for key, value in gyro_data.items())}")
+# Print accelerometer data
+print("Accelerometer Data:")
+for key, value in accel_data.items():
+    print(f"{key}: {value}")
+
+# Print gyroscope data
+print("Gyroscope Data:")
+for key, value in gyro_data.items():
+    print(f"{key}: {value}")
