eaa2b4498bb350c2fd19f92f1ba379dd1a135f49
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-01-24 22:52:08 +0100

    Update test_servo_movement.py

--- Software/test_servo_movement.py
+++ Software/test_servo_movement.py
@@ -4,7 +4,7 @@ from env.func.movement import Movement
 def test_servo_movement() -> None:
     m: Movement = Movement()
     # Test moving the right front thigh servo to various angles
-    angles_to_test: list[int] = [90, 40, 120, 150]  # Including an out-of-range angle (150)
+    angles_to_test: list[int] = [90, 40, 120]  # Including an out-of-range angle (150)
     for angle in angles_to_test:
         print(f"Moving servo to angle: {angle}")
         m.leg_right_front.thigh.move(target_angle=angle, duration=1.0)
