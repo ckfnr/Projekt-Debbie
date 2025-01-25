1b270c82c764297319f1d404bb59fd16c13505c7
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-01-25 14:57:50 +0100

    Add files via upload

--- Software/env/func/servos.py
+++ Software/env/func/servos.py
@@ -74,7 +74,9 @@ class ServoManager:
         # Adjust target angle and calculate the step difference
         if self.mirrored:
             # adjusted_target = (2 * config.servo_normal_position) - target_angle - self.deviation
-            adjusted_target = self.adjusted_normal_position * (1 + (1 - target_angle))
+            # adjusted_target = self.adjusted_normal_position * (1 + (1 - target_angle))
+            # adjusted_target = self.adjusted_normal_position + (self.adjusted_normal_position - target_angle)
+            adjusted_target = self.max_angle - (target_angle - self.min_angle)
         else:
             adjusted_target = target_angle + self.deviation
 
