500d2f6a620c3f595ac738b4e5a393306e145dfa
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-01-25 14:18:09 +0100

    Add files via upload

--- Software/env/func/servos.py
+++ Software/env/func/servos.py
@@ -54,8 +54,8 @@ class ServoManager:
         self.servo_channel: int = servo_channel
         # self.deviation: int = - deviation if mirrored else deviation
         self.deviation: int = deviation
-        self.min_angle: int = min_angle + self.deviation
-        self.max_angle: int = max_angle + self.deviation
+        self.min_angle: int = min_angle - self.deviation if mirrored else min_angle + self.deviation
+        self.max_angle: int = min_angle - self.deviation if mirrored else max_angle + self.deviation
         self.adjusted_normal_position: int = config.servo_normal_position + self.deviation
         self.calculation_angle: float = self.adjusted_normal_position
         self.mirrored: bool = mirrored
