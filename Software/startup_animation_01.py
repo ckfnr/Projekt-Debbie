ea5e97956becb3195b7dab343c58cf136dd603a4
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-01-25 19:39:06 +0100

    Update startup_animation_01.py

--- Software/startup_animation_01.py
+++ Software/startup_animation_01.py
@@ -10,22 +10,23 @@ m.normalize_all_legs()
 all_legs: list[Leg] = [m.leg_left_back, m.leg_left_front, m.leg_right_back, m.leg_right_front]
 
 def main() -> None:
-    # Lie down
-    for leg in all_legs:
-        leg.thigh.move(target_angle=120, duration=d)
-        leg.lower_leg.move(target_angle=80, duration=d)
-    # Join all threads
-    for leg in all_legs:
-        leg.thigh.join()
+    while True:
+        # Lie down
+        for leg in all_legs:
+            leg.thigh.move(target_angle=120, duration=d)
+            leg.lower_leg.move(target_angle=80, duration=d)
+        # Join all threads
+        for leg in all_legs:
+            leg.thigh.join()
 
-    # Stand up
-    for leg in all_legs:
-        leg.thigh.move(target_angle=40, duration=d)
-        leg.lower_leg.move(target_angle=120, duration=d)
-    # Join all threads
-    for leg in all_legs:
-        leg.thigh.join()
+        # Stand up
+        for leg in all_legs:
+            leg.thigh.move(target_angle=40, duration=d)
+            leg.lower_leg.move(target_angle=120, duration=d)
+        # Join all threads
+        for leg in all_legs:
+            leg.thigh.join()
 
-    print("Done!")
+        print("Done!")
 
 if __name__ == "__main__": main()
