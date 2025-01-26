3496f881a67880df9e954bdb84606a55ee8874ac
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-01-26 15:08:54 +0100

    Update config.py

--- Software/env/config.py
+++ Software/env/config.py
@@ -70,13 +70,13 @@ class Config:
                 "max_thigh": 120,
                 "min_lower_leg": 80,
                 "max_lower_leg": 120,
-                "min_side_axis": 60,  # 40
-                "max_side_axis": 110,  # 90
+                "min_side_axis": 70,  # 40
+                "max_side_axis": 120,  # 90
             },
             "deviations": {
                 "thigh": 12,
                 "lower_leg": -49,
-                "side_axis": 8,  # 48
+                "side_axis": -2,  # 48
             },
             "mirrored":{
                 "thigh": True,
