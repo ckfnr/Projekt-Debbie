6cabcbc82c7f42286f4a662c2d8f1eea59a32f1a
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-01-31 08:56:13 +0100

    Deleted useless files

--- Software/test_07.py
+++
@@ -1,9 +0,0 @@
-from env.func.movement import Movement
-
-m = Movement()
-
-m.normalize_all_legs()
-
-m.leg_right_front.thigh.set(target_angle=100, duration=0.5)
-m.leg_right_front.thigh.start()
-m.leg_right_front.thigh.join()
