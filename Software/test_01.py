158f6759a5b820d5010e958e37638b4bf39e44de
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-02-01 23:53:02 +0100

    Updated files

--- Software/test_01.py
+++ Software/test_01.py
@@ -1,13 +1,8 @@
-dic: dict[str, dict[str, dict[str, float]]] = {
-    'vectors':{
-        'rf': {'x': 0.0, 'y': 0.0, 'z': -5.0},
-        'rb': {'x': 0.0, 'y': 0.0, 'z': -5.0},
-        'lf': {'x': 0.0, 'y': 0.0, 'z': -5.0},
-        'lb': {'x': 0.0, 'y': 0.0, 'z': -5.0}
-        }
-    }
+from pprint import pprint
 
-# print([i for i in dic["vectors"].values()])
-# print([set(i) for i in dic["vectors"].values()])
-# print(any({"x", "y", "z"} != set(i) for i in dic["vectors"].values()))
-print(not all({"x", "y", "z"} == set(i.keys()) for i in dic["vectors"].values()))
+# Func
+from env.func.mmt_parser import parse_mmt
+
+instructions = parse_mmt("TEST_movement.mmt")
+
+pprint([i for i in instructions])
