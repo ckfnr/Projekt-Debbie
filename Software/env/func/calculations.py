8a50d7023a9caa91e97dc56e7213d2aefc498ee9
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-02-01 16:38:34 +0100

    Add files via upload

--- Software/env/func/calculations.py
+++ Software/env/func/calculations.py
@@ -7,4 +7,10 @@ from env.func.Classes import Coordinate
 
 @lru_cache(maxsize=50)
 def calc_circle_coordinates(step_width: float, coordinate_count: int) -> list[Coordinate]:
-    return []
+    """Calculates the coordinates of a circle with a specified step width and coordinate count."""
+    raise NotImplementedError("This function is not implemented yet!")
+
+@lru_cache(maxsize=50)
+def calc_ll_t_servo_angles(coordinate: Coordinate) -> tuple[int, int]:
+    """Calculates the lower_leg and thigh servo angles."""
+    raise NotImplementedError("This function is not implemented yet!")
