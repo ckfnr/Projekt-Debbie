4bef0f9e9be73a31c7cfabe92b93f597e900b66d
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2025-02-08 15:00:54 +0100

    updated main.py

--- Software/main.py
+++ Software/main.py
@@ -2,6 +2,22 @@ from env.func.movement import Movement
 
 def main() -> None:
     m = Movement()
+    d: float = 1
+
+    # Set legs
+    for leg in m.all_legs:
+        leg.thigh.set(target_angle=120, duration=d)
+        leg.lower_leg.set(target_angle=80, duration=d)
+
+    # Start legs
+    for leg in m.all_legs:
+        leg.thigh.start()
+        leg.lower_leg.start()
+
+    # Join legs
+    for leg in m.all_legs:
+        leg.thigh.join()
+        leg.lower_leg.join()
 
     m.normalize_all_legs()
 
